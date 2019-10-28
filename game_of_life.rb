8d5a52fdea9597460f5981df5171cd4e4c2869bb
Encrylize <encrylize@gmail.com>
2019-10-28 18:36:26 +0100

    Add grid frame and navigation

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,4 +1,5 @@
-require "curses"
+require 'curses'
+require 'forwardable'
 
 
 module CellState
@@ -7,17 +8,21 @@ module CellState
 end
 
 
-class GameOfLife
-  attr_reader :grid, :grid_dimensions
+class GameOfLifeGrid
+  include Enumerable
+  extend Forwardable
+
+  attr_reader :grid, :dimensions
+  def_delegators :@grid, :each, :[]
 
   def initialize(start_grid)
     @grid = start_grid
-    @grid_dimensions = {:height => @grid.size(), :width => @grid[0].size()}
+    @dimensions = {:height => @grid.size(), :width => @grid[0].size()}
   end
 
   def advance
-    new_grid = Array.new(grid_dimensions[:height]) do
-      Array.new(grid_dimensions[:width])
+    new_grid = Array.new(dimensions[:height]) do
+      Array.new(dimensions[:width])
     end
 
     new_grid.each_with_index do |line, y|
@@ -49,30 +54,78 @@ class GameOfLife
 
     return neighborhood
   end
+end
+
+
+class GridWindow < Curses::Window
+  attr_reader :grid, :grid_x, :grid_y
+  BOX_VERT_CHAR = '|'
+  BOX_HORIZ_CHAR = '-'
+  BOX_CORNER_CHAR = '*'
+
+  def initialize(height, width, top, left, grid)
+    super(height, width, top, left)
+    @grid = grid
+    @grid_x = 0
+    @grid_y = 0
+  end
+
+  def grid_x=(x)
+    @grid_x = x unless x < 0 or x > @grid.dimensions[:width] - maxx + 2
+  end
+
+  def grid_y=(y)
+    @grid_y = y unless y < 0 or y > @grid.dimensions[:height] - maxy + 2
+  end
+
+  def draw_horiz_frame
+    addch(BOX_CORNER_CHAR)
+    (maxx - 2).times { addch(BOX_HORIZ_CHAR) }
+    addch(BOX_CORNER_CHAR)
+  end
 
   def draw
-    Curses.clear
-    @grid.each do |line|
-      line.each {|cell| Curses.addch(cell == CellState::ALIVE ? 'O' : 'X')}
-      Curses.addch("\n")
+    clear
+    draw_horiz_frame
+
+    grid[@grid_y..@grid_y + maxy - 3].each do |line|
+      addch(BOX_VERT_CHAR)
+      line[@grid_x..@grid_x + maxx - 3].each do |cell|
+        addch(cell == CellState::ALIVE ? 'O' : 'X')
+      end
+      addch(BOX_VERT_CHAR)
     end
-    Curses.refresh
+
+    draw_horiz_frame
+    refresh
   end
 end
 
 
-gol = GameOfLife.new([[0, 0, 0, 0, 0],
-                      [0, 0, 1, 0, 0],
-                      [0, 0, 1, 0, 0],
-                      [0, 0, 1, 0, 0],
-                      [0, 0, 0, 0, 0]])
+grid = GameOfLifeGrid.new([[0, 0, 0, 0, 0],
+                           [0, 0, 1, 0, 0],
+                           [0, 0, 1, 0, 0],
+                           [0, 0, 1, 0, 0],
+                           [0, 0, 0, 0, 0]])
 
 Curses.init_screen
 begin
-  gol.draw
-  while Curses.getch
-    gol.advance
-    gol.draw
+  win = GridWindow.new(5, 5, 0, 0, grid)
+  win.draw
+  while ch = win.getch
+    case ch
+    when 'w'
+      win.grid_y -= 1
+    when 's'
+      win.grid_y += 1
+    when 'a'
+      win.grid_x -= 1
+    when 'd'
+      win.grid_x += 1
+    when 'u'
+      grid.advance
+    end
+    win.draw
   end
 ensure
   Curses.close_screen
